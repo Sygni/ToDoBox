8b68b1daf9b55e93782ad071369a1ad54593511b
sygni <sygni.grit@gmail.com>
2019-01-13 03:15:33 +0900

    Initial Commit

---
+++ ToDoBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoBox
+//
+//  Created by Identité on 13/01/2019.
+//  Copyright © 2019 Identité. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
